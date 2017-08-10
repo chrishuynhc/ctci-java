248099e0a28e401e106fd5008cd3b720a1f32f62
Chris <chris.huynh.c@gmail.com>
2017-08-09 23:45:58 -0500

    Working on it...

---
+++ Problem3_2.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Problem3_2 {
+	
+	static class Stack<T> {
+		
+		Node<T> head = null;
+		
+		public Stack(){
+			
+		}
+		
+		public T pop(){
+			
+		}
+		
+		public T push(){
+			
+		}
+		
+		public T min(){
+			
+		}
+	}
+
+	static class Node<T> {
+		Node next;
+		T data;
+		
+		public Node(T d){
+			this.data = d;
+		}
+	}
+}
