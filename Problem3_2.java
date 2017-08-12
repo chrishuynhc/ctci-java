6f460053922db2152a23817e3f4a48e7ce8bfc87
Chris <chris.huynh.c@gmail.com>
2017-08-11 19:07:54 -0500

    Fixed

--- Problem3_2.java
+++ Problem3_2.java
@@ -2,6 +2,26 @@ import java.util.*;
 
 public class Problem3_2 {
 	
+	public static void main(String[] args) {
+		
+	    Stack<Integer> stack = new Stack<Integer>();
+
+	    stack.push(1);
+	    stack.push(2);
+	    stack.push(3);
+	    stack.push(4);
+	    stack.push(5);
+	    stack.push(-1);
+	    stack.push(3);
+	    stack.push(4);
+
+	    System.out.println(stack.min() + " should be -1");
+	    System.out.println(stack.pop() + " should be 4");
+	    System.out.println(stack.pop() + " should be 3");
+	    System.out.println(stack.pop() + " should be -1");
+	    System.out.println(stack.min() + " should be 1");
+	}
+	
 	static class Stack<T> {
 		
 		Node<T> head = null;
@@ -29,10 +49,10 @@ public class Problem3_2 {
 				head = new Node<>(data);
 				min = new Node<>(head);
 			} else {
-				Node<T> node = new Node<>(data);
+				Node<T> node = new Node<T>(data);
 				if (((int) data) < ((int) min.data.data)) {
 					Node<Node<T>> nextMin = new Node<Node<T>>(node);
-					min.next = min;
+					nextMin.next = min;
 					min = nextMin;
 				}
 				node.next = head;
